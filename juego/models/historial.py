6dfb82eb4a727cca2d4e85e0f53465c4260a5c1c
Mrdavid656 <davidgemio98@gmail.com>
2021-10-20 21:55:18 -0400

    ft: crear modelo historial

--- juego/models/historial.py
+++ juego/models/historial.py
@@ -8,5 +8,5 @@ class Historial(models.Model):
     usuario = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='Historial_usuario')
     niveles_actividades = models.ForeignKey(
         NivelesActividades, on_delete=models.CASCADE, related_name='Historial_nivelesactividades')
-    puntos = models.DecimalField(max_length=4, decimal_places=1)
+    puntos = models.DecimalField(max_digits=4, decimal_places=1)
     like = models.BooleanField(default=False)
